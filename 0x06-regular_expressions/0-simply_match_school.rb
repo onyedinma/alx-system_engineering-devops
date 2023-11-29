6410bda54a3dcf2e2e6fe1aab791a5cf5eb5a823
onyedinma <onyedinmaonyekachi@yahoo.com>
2023-11-29 15:21:19 -0800

    Matching a string

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,12 +1,2 @@
 #!/usr/bin/env ruby
-# Get the first argument from the command line
-input = ARGV.first
-
-# Define a pattern to match the word School
-pattern = /School/
-
-# Check if the input is a valid string and contains the pattern
-if input.is_a?(String) && input.include?(pattern)
-  # Print the pattern
-  puts pattern
-end
+puts ARGV[0].scan(/School/).join

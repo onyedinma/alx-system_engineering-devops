67995f8d1c76277e04a1b5bb6a2f61861cb0918c
KachKreative <onyedinmaonyekachi@yahoo.com>
2024-02-04 18:04:10 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 #Installs puppet-lint
 
-package { 'puppet-lint':
-  ensure   => '2.1.0',
-  provider => 'gem',
+package { 'python3-pip':
+  ensure => present,  # Ensure pip3 is installed
+}
+
+python::pip { 'flask':
+  ensure       => '2.1.0',  # Specify version 2.1.0
+  pkgname      => 'Flask',  # Package name in pip
+  pip_provider => 'pip3',  # Use pip3 for installation
 }
